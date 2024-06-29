8c76a9e90259c60b93f37d60211b14b71784c34a
dev <hao286242341@gmail.com>
2024-06-29 17:37:03 +0800

    add fee

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,19 +3,20 @@ pragma solidity ^0.8.24;
 
 import "./PumpFun.sol";
 import "./Events.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PumpFunFactory {
+contract PumpFunFactory is Ownable {
     Events public eventsContract;
     address[] public deployedPumpFuns;
 
 
     event CreatePumpFun(address indexed token);
-    constructor() {
+    constructor() Ownable(msg.sender) {
         eventsContract = new Events();
     }
 
     function createPumpFun(string memory name, string memory symbol) public  {
-        PumpFun newPumpFun = new PumpFun(name, symbol, address(eventsContract));
+        PumpFun newPumpFun = new PumpFun(name, symbol, address(eventsContract), owner());
         deployedPumpFuns.push(address(newPumpFun));
         eventsContract.setIsPumpToken(address(newPumpFun), true);
         emit CreatePumpFun(address(newPumpFun));
--- contracts/PumpFun.sol
+++ contracts/PumpFun.sol
@@ -11,12 +11,14 @@ contract PumpFun is ERC20 {
     address payable public owner;
     uint256 constant public MAX_SUPPLY = 1000000000 ether;
     IEvents public events;
+    address public admin;
     event Withdrawal(uint amount, uint when);
     
 
-    constructor(string memory name, string memory symbol, address _events) ERC20(name, symbol){
+    constructor(string memory name, string memory symbol, address _events, address _admin) ERC20(name, symbol){
       _mint(address(this), MAX_SUPPLY);
       events = IEvents(_events);
+      admin = _admin;
 
     }
 
@@ -24,19 +26,25 @@ contract PumpFun is ERC20 {
 
     uint256 public tokensSold;
     uint256 public ethAmount;
+    uint256 public constant FEE_PERCENTAGE = 5; // 5% fee
 
     function buy() public payable {
         require(ethAmount + msg.value <= MAX_ETH_AMOUNT, "Bonding curve phase ended");
         
-        uint256 tokensToMint = calculateTokenAmount(msg.value);
-        ethAmount += msg.value;
+        uint256 fee = (msg.value * FEE_PERCENTAGE) / 100;
+        // Transfer fee to admin
+        (bool success, ) = payable(admin).call{value: fee}("");
+        require(success, "Fee transfer to admin failed");
+        uint256 ethAfterFee = msg.value - fee;
+        
+        uint256 tokensToMint = calculateTokenAmount(ethAfterFee);
+        ethAmount += ethAfterFee;
         require(tokensToMint > 0, "Not enough ETH sent");
 
         _transfer(address(this), msg.sender, tokensToMint);
         tokensSold += tokensToMint;
 
-
-        events.emitPumpFunEvents(true, msg.value, tokensToMint, ethAmount, tokensSold);
+        events.emitPumpFunEvents(true, ethAfterFee, tokensToMint, ethAmount, tokensSold);
         // if (tokensSold == BONDING_CURVE_SUPPLY) {
         //     // Transfer remaining tokens to Uniswap when bonding curve phase ends
         //     _transfer(address(this), address(this), UNISWAP_SUPPLY);
@@ -44,19 +52,24 @@ contract PumpFun is ERC20 {
         // }
     }
 
+
+    
     function sell(uint256 amount) public {
         require(tokensSold > 0, "No tokens sold yet");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
 
         uint256 ethToReturn = calculateEthAmount(amount);
-        require(address(this).balance >= ethToReturn, "Insufficient contract balance");
-
+        uint256 fee = (ethToReturn * FEE_PERCENTAGE) / 100;
+        // Transfer fee to admin
+        (bool success, ) = payable(admin).call{value: fee}("");
+        require(success, "Fee transfer to admin failed");
+        uint256 ethAfterFee = ethToReturn - fee > balanceOf(address(this)) ? balanceOf(address(this)) : ethToReturn - fee;
+        
         _transfer(msg.sender, address(this), amount);
         tokensSold -= amount;
-        (bool success, ) = payable(msg.sender).call{value: ethToReturn}("");
+        (success, ) = payable(msg.sender).call{value: ethAfterFee}("");
         require(success, "ETH transfer failed");
-        events.emitPumpFunEvents(false, ethToReturn, amount, ethAmount, tokensSold);
-
+        events.emitPumpFunEvents(false, ethAfterFee, amount, ethAmount, tokensSold);
     }
 
     function calculateTokenAmount(uint256 buyEthAmount) public view returns (uint256) {
