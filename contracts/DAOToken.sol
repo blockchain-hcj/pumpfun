d418d71829e03683966a8c9ed8ff8c20f2ca3a08
dev <hao286242341@gmail.com>
2024-06-30 12:49:13 +0800

    update

--- contracts/DAOToken.sol
+++ contracts/DAOToken.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DAOToken is ERC20, Ownable {
-    uint256 public constant TAX_RATE = 5; // 5% tax
+contract DAOToken is  ERC20, ERC20Permit, Ownable {
+    uint256 public  tax_rate = 5; // 5% tax
+    uint256 public constant MAX_SUPPLY =  177674000000 ether;
     mapping(address => bool) public isWhitelisted;
-
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+    
+    constructor() ERC20("MPAA", "MPAA") ERC20Permit("MPAA") Ownable(msg.sender) {
+        _mint(msg.sender, MAX_SUPPLY);
     }
 
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
@@ -26,7 +28,7 @@ contract DAOToken is ERC20, Ownable {
         if (isWhitelisted[sender] || isWhitelisted[recipient]) {
             _transfer(sender, recipient, amount);
         } else {
-            uint256 taxAmount = (amount * TAX_RATE) / 100;
+            uint256 taxAmount = (amount * tax_rate) / 100;
             uint256 amountAfterTax = amount - taxAmount;
             _transfer(sender, owner(), taxAmount);
             _transfer(sender, recipient, amountAfterTax);
