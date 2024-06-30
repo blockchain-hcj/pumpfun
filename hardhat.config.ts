93ae6b5fbf8a65779d2e59eb4c947e777a2fab44
dev <hao286242341@gmail.com>
2024-06-30 20:32:08 +0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,18 +4,29 @@ import { Secrets } from "./secrets";
 import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10,
+          },
+        },
+      },]
+    },
   networks: {
     arbSepolia: {
       url: "https://arb-sepolia.g.alchemy.com/v2/I-ZVEdUQy4Mk3rwbsNAIp_MVql6coseO",
       accounts: [Secrets.DEPLOYER_PRIVATEKEY, Secrets.TEST1],
     },
     baseSepolia:{
-      url: "https://base-mainnet.g.alchemy.com/v2/0cNsvrP9a82KWY24wOyUVpgKf8T7WJKQ",
+      url: "https://base-sepolia.g.alchemy.com/v2/0cNsvrP9a82KWY24wOyUVpgKf8T7WJKQ",
       accounts: [Secrets.DEPLOYER_PRIVATEKEY, Secrets.TEST1],
     }
   },
-  defaultNetwork: "baseSepolia",
+  defaultNetwork: "arbSepolia",
 
 };
 
