19542e45c672ebc2ed47527bcee0f0232692f9ce
dev <hao286242341@gmail.com>
2024-09-06 17:23:52 +0800

    v3

--- contracts/PumpFun.sol
+++ contracts/PumpFun.sol
@@ -6,23 +6,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
 import "./interfaces/IEvents.sol";
-import "./interfaces/IUniswapV2Router.sol";
-import "./interfaces/IUniswapV2Factory.sol";
-import "./interfaces/IUniswapPair.sol";
+import "./interfaces/INonfungiblePositionManager.sol";
 import "./interfaces/IFactory.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-
-
 
 contract PumpFun is ERC20, ReentrancyGuard {
 
     address payable public owner;
     uint256 constant public MAX_SUPPLY = 1000000000 ether;
-    address constant public UNISWAP_V2_ROUTER = 0xA3C957B20779Abf06661E25eE361Be1430ef1038;
-    address constant public UNISWAP_V2_FACTORY = 0x31a78894a2B5dE2C4244cD41595CD0050a906Db3;
+    address constant public NONFUNGIBLE_POSITION_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
     IEvents public events;
     bool public isPaused;
-    bool public initialized;
     IFactory public factory;
     uint256 public constant MAX_ETH_AMOUNT = 15 ether;
 
@@ -34,24 +28,24 @@ contract PumpFun is ERC20, ReentrancyGuard {
 
     constructor(string memory name, string memory symbol, address _events, address creator) ERC20(name, symbol) payable{
          events = IEvents(_events);
-        _mint(address(this), MAX_SUPPLY);
         factory = IFactory(msg.sender);
+        _mint(address(this), MAX_SUPPLY);
         if(msg.value > 0){
-            buy_internal(creator, msg.value);
+            buy(creator, msg.value, 0);
         }
     }
 
   
 
-    function buy(uint256 slippage) public payable nonReentrant{
+    function buy(address receiver, uint256 buyEthAmount, uint256 slippage) public payable nonReentrant{
 
         require(!isPaused, "Bonding curve phase ended");
 
-        uint256 fee = (msg.value * FEE_PERCENTAGE) / 100;
+        uint256 fee = (buyEthAmount * FEE_PERCENTAGE) / 100;
         //Transfer fee to admin
        (bool success, ) = payable(factory.feeReceiver()).call{value: fee}("");
         require(success, "Fee transfer to feeReceiver failed");
-        uint256 ethAfterFee = msg.value - fee;
+        uint256 ethAfterFee = buyEthAmount - fee;
       
         uint256 tokensToMint = calculateTokenAmount(ethAfterFee);
         
@@ -60,57 +54,40 @@ contract PumpFun is ERC20, ReentrancyGuard {
         uint256 expectedPrice = getCurrentTokenPrice();
         uint256 maxAcceptablePrice = expectedPrice * (10000 + slippage) / 10000;
         if(ethAmount != 0 ){
-        require(actualPrice <= maxAcceptablePrice, "Price exceeds allowed slippage");
+            require(actualPrice <= maxAcceptablePrice, "Price exceeds allowed slippage");
         }
         ethAmount += ethAfterFee;
         require(ethAmount <= MAX_ETH_AMOUNT, "Max ETH amount reached");
         require(tokensToMint > 0, "Not enough ETH sent");
 
-        _transfer(address(this), msg.sender, tokensToMint);
+        _transfer(address(this), receiver, tokensToMint);
         tokensSold += tokensToMint;
-        events.emitPumpFunEvents(msg.sender, true, ethAfterFee, tokensToMint, ethAmount, tokensSold, getCurrentTokenPrice());
-       if(ethAmount >= MAX_ETH_AMOUNT){
-            isPaused = true;
-            // add to uniswap
-        _approve(address(this), UNISWAP_V2_ROUTER, balanceOf(address (this)));
-
-        IUniswapV2Router(UNISWAP_V2_ROUTER).addLiquidityETH{value: address(this).balance}(
-            address(this), balanceOf(address(this)), 0, 0, address(0), block.timestamp
-        );
-            
-       }
-    }
-
-    function buy_internal(address receiver, uint256 buyEthAmount) public  {
-        require(!initialized, "PumpFun already initialized");
-       
-        require(!isPaused, "Bonding curve phase ended");
-
-        uint256 fee = (buyEthAmount * FEE_PERCENTAGE) / 100;
-        // Transfer fee to admin
-        (bool success, ) = payable(factory.feeReceiver()).call{value: fee}("");
-        require(success, "Fee transfer to feeReceiver failed");
-        uint256 ethAfterFee = buyEthAmount - fee;
-        
-        uint256 tokensToMint = calculateTokenAmount(ethAfterFee);
-        ethAmount += ethAfterFee;
-        require(tokensToMint > 0, "Not enough ETH sent");
-
-        _transfer(address(this),receiver, tokensToMint);
-        tokensSold += tokensToMint;
-
         events.emitPumpFunEvents(receiver, true, ethAfterFee, tokensToMint, ethAmount, tokensSold, getCurrentTokenPrice());
-       if(ethAmount >= MAX_ETH_AMOUNT){
-            isPaused = true;
-            // add to uniswap
-        _approve(address(this), UNISWAP_V2_ROUTER, balanceOf(address (this)));
 
-        IUniswapV2Router(UNISWAP_V2_ROUTER).addLiquidityETH{value: address(this).balance}(
-            address(this), balanceOf(address(this)), 0, 0, address(0), block.timestamp
-        );
+        if(ethAmount >= MAX_ETH_AMOUNT){
+            isPaused = true;
+            // add liquidity to Uniswap V3
+            _approve(address(this), NONFUNGIBLE_POSITION_MANAGER, balanceOf(address(this)));
             
+            INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
+                token0: address(this),
+                token1: address(0), // ETH
+                fee: 3000, // 0.3% fee tier
+                tickLower: -887220,
+                tickUpper: 887220,
+                amount0Desired: balanceOf(address(this)),
+                amount1Desired: address(this).balance,
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: address(this),
+                deadline: block.timestamp
+            });
+
+            INonfungiblePositionManager(NONFUNGIBLE_POSITION_MANAGER).mint{value: address(this).balance}(params);
        }
-          initialized = true;
+       
+
+       
     }
 
     function getMaxEthToBuy() public view returns (uint256) {
@@ -158,9 +135,17 @@ contract PumpFun is ERC20, ReentrancyGuard {
         events.emitPumpFunEvents(msg.sender, false, ethAfterFee, amount, ethAmount, tokensSold, getCurrentTokenPrice());
     }
 
+ 
+
+    function getCoefficient() internal pure returns (uint256) {
+        // Calculate the coefficient based on MAX_ETH_AMOUNT and MAX_SUPPLY
+        // tokenAmount = co * ethAmount^2, where tokenAmount = 0.8 * MAX_SUPPLY and ethAmount = MAX_ETH_AMOUNT
+        // Solving for co: co = (0.8 * MAX_SUPPLY) / MAX_ETH_AMOUNT^2
+        return (206559139 * 10**9)**2;
+    }
+
     function calculateTokenAmount(uint256 buyEthAmount) public view returns (uint256) {
-        uint256 supply = tokensSold;
-        uint256 tokenAmount = 206559139 * (10 ** 9 ) * (Math.sqrt(buyEthAmount + ethAmount) - Math.sqrt(ethAmount));
+        uint256 tokenAmount = getCoefficient() * (Math.sqrt(buyEthAmount + ethAmount) - Math.sqrt(ethAmount));
         return tokenAmount;
     }
     // y = 206559139 * (10 ** 9 ) * sqrt(x)
@@ -169,8 +154,8 @@ contract PumpFun is ERC20, ReentrancyGuard {
         uint256 supply = tokensSold;
         uint256 newSupply = supply - tokenAmount;
         // Using the inverse of the formula: x = (y / (206559139 * 10^9))^2 - ethAmount
-        uint256 currentEth = (supply * 1 ether  / (206559139 * 10**9))**2 / 1 ether / 1 ether ;
-        uint256 newEth = (newSupply * 1 ether  / (206559139 * 10**9))**2  / 1 ether / 1 ether   ;
+        uint256 currentEth = (supply * 1 ether  / getCoefficient()) **2 / 1 ether / 1 ether ;
+        uint256 newEth = (newSupply * 1 ether  / getCoefficient()) **2  / 1 ether / 1 ether   ;
         
         return currentEth - newEth;
     }
@@ -180,12 +165,13 @@ contract PumpFun is ERC20, ReentrancyGuard {
         super._update(from, to, value);
         events.emitPumpFunTransfer(from, to, value);
     }
+    
 
 
     function getCurrentTokenPrice() public view returns (uint256) {
 
         // We need to calculate this at the current ethAmount
-        uint256 coefficient = 206559139 * (10 ** 9);
+        uint256 coefficient = getCoefficient();
         uint256 sqrtEthAmount = Math.sqrt(ethAmount);
 
         // To maintain precision, we'll multiply by 1e18 and then divide
--- test/deploy.t.sol
+++ test/deploy.t.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.8.0;
 import "forge-std/Test.sol";
 import "../contracts/PumpFunFactory.sol";
 import "../contracts/interfaces/IPumpFun.sol";
-import "../contracts/interfaces/IUniswapV2Router.sol";
+
 
 contract EmissionTest is Test {
 
@@ -18,7 +18,7 @@ contract EmissionTest is Test {
         vm.deal(user1, 10000 ether);
 
         vm.startPrank(user1);
-        factory = PumpFunFactory(0xcB8327E07D377F55cb251F60108bC9163d72Cca4);
+        factory = new PumpFunFactory();
     
 
 
@@ -26,22 +26,26 @@ contract EmissionTest is Test {
 
     function testCreateToken() public{
 
-        factory.createPumpFun("PumpFun", "PFP");
-
+        factory.createPumpFun{value: 15 ether}("PumpFun", "PFP");
+        
         address[] memory tokens = factory.getDeployedPumpFuns();
-            IPumpFun(tokens[0]).buy{value: 1 ether}(10000);
+        uint256 contractBalance = IPumpFun(tokens[0]).balanceOf(user1);
+        uint256 maxEthToBuy = IPumpFun(tokens[0]).getMaxEthToBuy();
+        console.log(maxEthToBuy);
 
-            IPumpFun(tokens[0]).buy{value: 0.1 ether}(500);
+            // IPumpFun(tokens[0]).buy{value: 1 ether}(10000);
 
-            // assertEq(balance1 < balance2, true, "Balance 1 is not less than balance 2");
-            // assertEq(balance1 > 0, true, "Balance 1 is not greater than 0");    
-            // assertEq(balance2 > 0, true, "Balance 2 is not greater than 0");
-            uint256 maxEthToBuy = IPumpFun(tokens[0]).getMaxEthToBuy();
-            uint256 contractBalance = address(tokens[0]).balance;
-            IPumpFun(tokens[0]).buy{value: maxEthToBuy}(1 ether);
-            uint256 contractBalance2 = address(tokens[0]).balance;
-            console.log(contractBalance);
-            console.log(contractBalance2);
+            // IPumpFun(tokens[0]).buy{value: 0.1 ether}(500);
+
+            // // assertEq(balance1 < balance2, true, "Balance 1 is not less than balance 2");
+            // // assertEq(balance1 > 0, true, "Balance 1 is not greater than 0");    
+            // // assertEq(balance2 > 0, true, "Balance 2 is not greater than 0");
+            // uint256 maxEthToBuy = IPumpFun(tokens[0]).getMaxEthToBuy();
+            // uint256 contractBalance = address(tokens[0]).balance;
+            // IPumpFun(tokens[0]).buy{value: maxEthToBuy}(1 ether);
+            // uint256 contractBalance2 = address(tokens[0]).balance;
+            // console.log(contractBalance);
+            // console.log(contractBalance2);
 
             // uint256 balance = IERC20(tokens[0]).balanceOf(user2);
             // console.log(1 ether * 1 ether / balance);
@@ -52,9 +56,6 @@ contract EmissionTest is Test {
             // console.log(IPumpFun(tokens[0]).isPaused());
             // console.log(IPumpFun(tokens[0]).getCurrentTokenPrice());
 
-        IUniswapV2Factory uniswapFactory = IUniswapV2Factory(0x31a78894a2B5dE2C4244cD41595CD0050a906Db3);
-        address pair = uniswapFactory.getPair(address(tokens[0]), 0x0Dc808adcE2099A9F62AA87D9670745AbA741746);
-        console.log(pair);
     }
     
 
